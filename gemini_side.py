1c2ff5fe1d01fffe378ee2ffcd73b8efa0148233
Maria Guarecuco <mgabriela.guarecuco@gmail.com>
2023-12-20 23:32:58 -0400

    try a different prompt and lower temperature

--- gemini_side.py
+++ gemini_side.py
@@ -15,52 +15,40 @@ import PIL.Image
 from dotenv import load_dotenv
 
 load_dotenv()
+
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-pro-vision")
 
 
 def get_gemini_response(image_path=None):
-    # response = input("Just for testing, tell me... ")
-    # return response
-
-    prompt = 'Imagine you are a Tetris player with superhuman abilities. You have the power to see 5 moves ahead and can instantly calculate the best possible move for any given board state. You also have perfect hand-eye coordination and can execute any move with precision.\n\nYou will be given a series of screenshots, each representing a different board state in the game. For each screenshot, you should output an integer representing the best move to make. The possible moves are:\n\nLeft: 1\nRight: 2\nDown: 3\nDrop: 4\nRotate clockwise: 5\nRotate counterclockwise: 6\n\nThe board state is represented by a grid of 10 columns and 20 rows. Each cell in the grid can be empty or contain a Tetris block. The Tetris blocks are represented by different colors and are called tetraminoes. At the right of the screen, you can see the tetraminoes that will fall next.\n\nThe game starts with a random sequence of Tetris blocks falling from the top of the screen. You can move the falling block left or right, and also rotate it clockwise or counterclockwise. You can also drop the block immediately to the bottom.\n\nWhen a complete row of blocks is formed, it disappears and the blocks above it fall down. Points are scored for each row that is cleared. The game ends when the blocks reach the top of the screen.\n\nYour goal is to play Tetris and achieve the highest possible score by maximizing cleared lines and minimizing block gaps. It\'s important to prioritize moves that achieve the scoring objective first, then prioritize filling gaps, and lastly consider immediate dropping to keep the game flowing.\n\nStructure your response as a JSON, so as {"action" : NUMBER} where NUMBER is an integer representing the best move to make for each screenshot. Your response should start with { and end with }.'
+    prompt = 'Imagine you are a Tetris player with superhuman abilities. You have the power to see 5 moves ahead and can instantly calculate the best possible move for any given board state. You also have perfect hand-eye coordination and can execute any move with precision.\n\nI am presenting you with an image of a Tetris board at its current state. The board is represented by a grid of 10 columns and 10 rows. The empty cells are grey while the Tetrominoes are represented by red color. The possible moves are:\n\nLeft: "left"\nRight: "right"\nDown: "down"\nDrop: "drop"\nRotate clockwise: "turn right"5\nRotate counterclockwise: "turn left"\n\nThe game starts with a random Tetromino falling from the top of the board. You can move the falling Tetromino left or right, and also rotate it clockwise or counterclockwise. You can also drop the Tetromino immediately to the bottom.\n\nWhen a complete row of blocks is formed, it disappears and the blocks above it fall down. Points are scored for each row that is cleared. The game ends when the blocks reach the top of the board.\n\nYour goal is to play Tetris and achieve the highest possible score by maximizing cleared lines and minimizing block gaps. Let\'s think step by step:\n1. Analyze the board, the position of the current blocks and where the current tetromino would best slot in.\n2. Rank the possible moves considering the current state of the board. Prioritize moves that achieve line clears, then prioritize filling gaps. Lastly, consider immediate dropping to keep the game flowing.\n3. Choose the best move based on the analysis. Do not chose a move before doing all the steps to make sure you have the best possible move.\n\nStructure your response as a JSON, so as {"board_state", BOARD_STATE, "tetromino": TETROMINO, "explanation": EXPLANATION, "action": ACTION} where BOARD_STATE is the current state of the board described in 1-3 sentences, TETROMINO is the type of the falling tetromino (I, J, L, O, S, T, Z), EXPLANATION is your reasoning behind the move and ACTION is the string representing the chosen move. Do not add any more keys to the JSON. Your response should start with { and end with }.'
 
     img = PIL.Image.open(image_path)
-    model = genai.GenerativeModel("gemini-pro-vision")
-    response = model.generate_content([prompt, img], stream=False)
+    response = model.generate_content(
+        contents=[img, prompt],
+        generation_config=genai.types.GenerationConfig(
+            # max_output_tokens=500,
+            temperature=0.2,
+        ),
+        stream=False,
+    )
 
     text = response.text
-    print(f"Gemini response: {text}")
+    print(text, "\n")
     stripped_text = text[text.index("{") : (text.index("}") + 1)]
-    print(f"Stripped text: {stripped_text}")
     json = eval(stripped_text)
-    action = json["action"]
+    return json["action"]
 
-    action_mapping = {
-        1: "left",
-        2: "right",
-        3: "down",
-        4: "drop",
-        5: "turn right",
-        6: "turn left",
-    }
-    action_text = action_mapping.get(action)
-    print(f"Action: {action_text}")
-    return action_text
-
-
-state_counter = 1
 
 if os.path.exists("screens"):
-    files = os.listdir("screens")
-    for file in files:
-        if file != "screenshot_0.png":
-            os.remove(os.path.join("screens", file))
-else:
-    os.mkdir("screens")
+    shutil.rmtree("screens")
+os.mkdir("screens")
 
 if os.path.exists("actions"):
     shutil.rmtree("actions")
 os.mkdir("actions")
+
+state_counter = 1
 
 while True:
     time.sleep(1)
